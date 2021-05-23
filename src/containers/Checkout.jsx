24d25fe4f9279264295625c403556272a3978fae
Jader Cano <jader.cano@contractors.accruent.com>
2021-05-23 09:22:00 -0500

    Added FontAwesome Icons.

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Checkout.css';
 
 const Checkout = () => (
@@ -10,12 +11,16 @@ const Checkout = () => (
           <h4>Item name</h4>
           <span>$ price</span>
         </div>
-        <button type="button">Eliminar</button>
+        <button type="button">
+          <i className="fas fa-trash-alt" />
+        </button>
       </div>
     </div>
     <div className="Checkout-sidebar">
       <h3>Precio total: 10$</h3>
-      <button type="button">Continuar pedido</button>
+      <Link to="/checkout/information">
+        <button type="button">Continuar pedido</button>
+      </Link>
     </div>
   </div>
 );
